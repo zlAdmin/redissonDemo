b41bb93076ccbfc988db3c0a6de06504f28fb5ba
zlAdmin <13006315738@163.com>
2019-09-15 14:43:58 +0800

    修改java版本

--- src/main/java/com/zl/controller/RedissonController.java
+++ src/main/java/com/zl/controller/RedissonController.java
@@ -36,18 +36,18 @@ public class RedissonController {
                 @Override
                 public void run() {
                     try {
-                        System.err.println("=============线程开启============"+Thread.currentThread().getName());
-					/*distributedLocker.lock(key,10L); //直接加锁，获取不到锁则一直等待获取锁
-					 Thread.sleep(100); //获得锁之后可以进行相应的处理
-					 System.err.println("======获得锁后进行相应的操作======"+Thread.currentThread().getName());
-					 distributedLocker.unlock(key);  //解锁
-					 System.err.println("============================="+Thread.currentThread().getName());*/
-                        boolean isGetLock =  distributedLocker.tryLock(key, TimeUnit.SECONDS,5L,10L); //尝试获取锁，等待5秒，自己获得锁后一直不解锁则10秒后自动解锁
-                        if(isGetLock){
+                        System.err.println("=============线程开启============" + Thread.currentThread().getName());
+                        /*distributedLocker.lock(key, 10L); //直接加锁，获取不到锁则一直等待获取锁
+                        Thread.sleep(100); //获得锁之后可以进行相应的处理
+                        System.err.println("======获得锁后进行相应的操作======" + Thread.currentThread().getName());
+                        distributedLocker.unlock(key);  //解锁
+                        System.err.println("=============================" + Thread.currentThread().getName());*/
+                        boolean isGetLock = distributedLocker.tryLock(key, TimeUnit.SECONDS, 5L, 10L); //尝试获取锁，等待5秒，自己获得锁后一直不解锁则10秒后自动解锁
+                        if (isGetLock) {
                             Thread.sleep(100); //获得锁之后可以进行相应的处理
-                            System.err.println("======获得锁后进行相应的操作======"+Thread.currentThread().getName());
+                            System.err.println("======获得锁后进行相应的操作======" + Thread.currentThread().getName());
                             //distributedLocker.unlock(key);
-                            System.err.println("============================="+Thread.currentThread().getName());
+                            System.err.println("=============================" + Thread.currentThread().getName());
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -62,7 +62,7 @@ public class RedissonController {
     @ResponseBody
     public void redissonTestC() throws InterruptedException {
         RLock lock = distributedLocker.lock(key);
-        a ++;
+        a++;
         Thread.sleep(1000L);
         lock.unlock();
     }
